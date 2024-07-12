7a5d10aa53b2cb894da58ca41cbde55bf134e4e1
sohail808 <sohailworks10@gmail.com>
2024-07-12 20:33:50 +0530

    backend url changed

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BACKEND_URL = "http://127.0.0.1:8787";
 export const BACKEND_URL = "https://backend.sohailatwork10.workers.dev";
+// export const BACKEND_URL = "http://127.0.0.1:8787";
 
 export const api = axios.create({
   baseURL: BACKEND_URL,
